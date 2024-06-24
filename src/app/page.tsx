02e47a68bf4dd74367a0e3e68fc70d40537e8b48
jlmartinez123 <jlmartinez@morepower.ph>
2024-06-24 15:10:21 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import { FormEventHandler, useState } from "react";
 
 export default function Home() {
 
+  //component (test lang ni)
+  // const data = [{name:'john doe', age: 31}, {name:'jane doe', age: 21}, {name:'jake', age: 34}]
+
   //Form variables(?)
   const [firstName, setFirstName] = useState(" "); 
   const [lastName, setLastName] = useState(" "); 
@@ -19,10 +22,22 @@ export default function Home() {
   }
 
   return <div className=" max-w-full h-screen flex items-center justify-center flex-col gap-6">
-    <div className="bg-gradient-to-tl from-indigo-800 to-purple-800 rounded-3xl pt-8 pb-4 pl-8 pr-8 bg-transparent space-x-1.5">
+    
+    {/* component part 2 (Test lang ni) */}
+    {/* {data.map((item, index) => (
+      <div className="p-5 bg-blue-600 rounded-md" key={index}>
+        <text> {item.age} {item.name}</text>
+      </div>
+        )
+      )
+    
+    } */}
+
+
+    <div className=" bg-opacity-20 bg-gradient-to-tl from-red-900 to-purple-800 rounded-3xl pt-8 pb-4 pl-8 pr-8 space-x-1.5 ring-white">
       
       <h1 className="text-white text-lg text-center"> Contact Form</h1> 
-      <hr/>
+      <hr className="opacity-40"/>
 
       <br/>
 
@@ -41,14 +56,14 @@ export default function Home() {
         <br/>
         
         <div className="flex justify-end">
-          <input type="submit" className="border white py-[8px] px-[16px] rounded-[8px] bg-blue-500 self-end"/>
+          <input type="submit" className=" border white py-[8px] px-[16px] rounded-[8px] bg-blue-500 self-end hover:bg-blue-700 bg-gradient-to-l from-blue-700 to-purple-600"/>
         </div>
 
       </form>
     
     </div>
 
-    <button className="border white py-[8px] px-[16px] rounded-[8px] bg-blue-500" onClick={()=>(
+    <button className=" border-white py-[8px] px-[16px] rounded-[8px] bg-blue-500" onClick={()=>(
     window.alert("Hello World!")
   )
     
