d0daae5a8be1491b262f9a8be53957769f678dac
jlmartinez123 <jlmartinez@morepower.ph>
2024-06-24 13:31:11 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@ import Image from "next/image";
 
 export default function Home() {
   return <div className=" max-w-full h-screen flex items-center justify-center">
+    
+
+    <p>Lorem</p>
     <button className="border white py-[8px] px-[16px] rounded-[8px] bg-blue-500" onClick={()=>(
     window.alert("Hello World!")
   )
-    
   }>Click Me!</button>
+  
 </div>
 
 }
