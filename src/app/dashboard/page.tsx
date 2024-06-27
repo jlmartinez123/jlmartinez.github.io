f9bb919c4ce560899ae123634744d893e75adeec
jlmartinez123 <jlmartinez@morepower.ph>
2024-06-27 14:10:39 +0800

    refactor: add buttons

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,7 +14,34 @@ function dashboardPage(){
         <div className="flex flex-row w-screen h-screen bg-white">
 
             {/* Navigation Bar */}
-            <div className="w-[260px] h-full bg-green-100">
+
+            <div className="w-[260px] h-full bg-green-100 px-[16px] py-[32px]">
+                <img src="assets/mpulogo.png" alt="MORE PowerUp Logo"/>
+
+                {/* Navigation items */}
+                <div className="pt-[48px] space-y-[8px] text-left">
+
+                    <button className="w-full text-white font-sans font-normal text-[14px] leading-[124%] tracking-[-3%] py-2 text-left">Dashboard</button>
+
+                    <button className="w-full text-white font-sans font-normal text-[14px] leading-[124%] tracking-[-3%] py-2 text-left">Transactions</button>
+
+                    <button className="w-full text-white font-sans font-normal text-[14px] leading-[124%] tracking-[-3%] py-2 text-left">Outages</button>
+
+                    <button className="w-full text-white font-sans font-normal text-[14px] leading-[124%] tracking-[-3%] py-2 text-left">Alerts</button>
+
+                    <button className="w-full text-white font-sans font-normal text-[14px] leading-[124%] tracking-[-3%] py-2 text-left">Update Account</button>
+
+                    <button className="w-full text-white font-sans font-normal text-[14px] leading-[124%] tracking-[-3%] py-2 text-left">Services</button>
+
+                    <button className="w-full text-white font-sans font-normal text-[14px] leading-[124%] tracking-[-3%] py-2 text-left">About MORE PowerUp</button>
+
+                    <button className="w-full text-white font-sans font-normal text-[14px] leading-[124%] tracking-[-3%] py-2 text-left">Privacy Policy</button>
+
+                    <button className="w-full text-white font-sans font-normal text-[14px] leading-[124%] tracking-[-3%] py-2 text-left">Terms and Conditions</button>
+
+                    <button className="w-full text-white font-sans font-normal text-[14px] leading-[124%] tracking-[-3%] py-2 text-left">Contact Us</button>
+
+                </div>
 
 
             </div>
